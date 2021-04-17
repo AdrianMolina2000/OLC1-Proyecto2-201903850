6071ced84aaadef2cf5b3aae4e5a55430c259212
Adrian Samuel Molina Cabrera <64050699+AdrianMolina2000@users.noreply.github.com>
2021-04-16 22:10:49 -0600

    reordenamiento de proyecto

---
+++ src/controllers/principal.controller.ts
@@ -0,0 +1,51 @@
+import {Request,Response} from 'express';
+import Excepcion from './analizador/Excepciones/Excepcion';
+import Arbol from './analizador/tablaSimbolos/Arbol';
+import tablaSimbolos from './analizador/tablaSimbolos/tablaSimbolos';
+
+var Errors:Array<Excepcion> = new Array<Excepcion>();
+
+class principalController{
+
+    public helloWorld (req:Request,res:Response){
+        res.send("Hola Mundo ");
+    }
+    
+    public interpretar (req:Request,res:Response){
+        var parser = require('./analizador/principal');
+
+        const text = req.body.entrada;
+
+        try{
+            let ast = new Arbol( parser.parse(text) );
+
+            var tabla = new tablaSimbolos();
+            ast.setGlobal(tabla);
+
+            for(let m of ast.getInstrucciones()){
+
+                if(m instanceof Excepcion){ // ERRORES SINTACTICOS
+                    Errors.push(m);
+                    ast.updateConsola((<Excepcion>m).toString());
+                }
+                var result = m.interpretar(ast, tabla);
+                if(result instanceof Excepcion){ // ERRORES SINTACTICOS
+                    Errors.push(result);
+                    ast.updateConsola((<Excepcion>result).toString());
+                }
+            }
+            res.json({consola:ast.getConsola(), Errores: Errors});
+        }
+        catch(err){
+            console.log(err);
+            res.json({
+                salida : err,
+                errores : err
+            });
+        }
+    }
+    
+}   
+
+export const controller = new principalController();
+
